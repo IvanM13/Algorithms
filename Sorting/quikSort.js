b91b10e01db8e1ec7682e9f76c18b4d483a5c5b4
Ivan M <slade91@mail.ru>
2024-04-08 20:14:24 +0300

    mod quick

--- Sorting/quikSort.js
+++ Sorting/quikSort.js
@@ -15,8 +15,45 @@ function quickSort(arr){
     return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
+function quickSort2(items) {
+    return quickSortHelper(items, 0, items.length - 1);
+}
 
+function quickSortHelper(items, left, right) {
+    let index;
+    if (items.length > 1) {
+        index = partition(items, left, right);
+
+        if (left < index - 1) {
+            quickSortHelper(items, left, index - 1);
+        }
+        if (index < right) {
+            quickSortHelper(items, index, right);
+        }
+    }
+    return items;
+}
+
+function partition(array, left, right) {
+    let pivot = array[Math.floor((right + left) / 2)];
+    while(left <= right){
+        while (pivot > array[left]) {
+            left++;
+        }
+        while (pivot < array[right]) {
+            right--;
+        }
+        if (left <= right) {
+            let temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    return left;
+}
 
 mass = [4,1,6,7,9,3,2,8,5];
 
-console.log(quickSort(mass));
+console.log(quickSort2(mass));
